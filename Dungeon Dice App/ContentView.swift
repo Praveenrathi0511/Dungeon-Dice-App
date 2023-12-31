9a0a08dbba245d5527cd11c48f880492ab7d69bc
Praveenrathi0511 <praveenrathik11@gmail.com>
2023-12-31 13:25:04 +0530

    Initial Commit

---
+++ Dungeon Dice App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dungeon Dice App
+//
+//  Created by Praveen rathi on 10/10/1945 Saka.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dungeon Dice App/Dungeon_Dice_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dungeon_Dice_AppApp.swift
+//  Dungeon Dice App
+//
+//  Created by Praveen rathi on 10/10/1945 Saka.
+//
+
+import SwiftUI
+
+@main
+struct Dungeon_Dice_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
